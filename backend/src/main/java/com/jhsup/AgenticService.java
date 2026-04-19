a9c0021d75c712b0f38e0a2050409f3930480434
JohnnyHuang101 <123030055+JohnnyHuang101@users.noreply.github.com>
2026-04-19 10:19:53 -0700

    some stuff

---
+++ backend/src/main/java/com/jhsup/AgenticService.java
@@ -0,0 +1,16 @@
+package com.jhsup;
+
+
+@Service 
+public class AgenticService {
+    
+    private List<String> Queries;
+    private final ChatClient chatClient;
+    private final String to create a study plan for the student. first laye rof agentic behavior is to obtain the exam datesas well as the topics. if explicitly stated then happens
+    have the exam dates include their respective units. all units must be included before last exam. if not specified divide up into how ever many test segments evenly.
+
+    resposne with as man ylines as there are midterms for the units and dates etc other meta information in a json format.
+
+    
+    public 
+}
